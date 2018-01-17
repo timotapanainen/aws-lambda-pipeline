60e80c654b0b3fd6b71e4c5fc4c80bdd52a26b05
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-01-17 23:11:04 +0200

    codebuild buildspec

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import json
 
 print("loading function")
 
+
 def handler(event, context):
     print(json.dumps(event))
-    return "Hello World"
+    return {"statusCode": 200,
+            "body": "Hello World!"}
